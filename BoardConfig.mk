4f966eb9ebdb66dc0874a547965df582bbd2518f
PeterCxy <xqsx43cxy@126.com>
2013-10-20 14:27:42 +0800

    mione_plus: get back to ClockWorkMod

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,15 +70,11 @@ BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 # Recovery
 BOARD_USES_MMCUTILS := true
 BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
 TARGET_RECOVERY_INITRC := device/xiaomi/mione_plus/recovery/init.rc
-RECOVERY_HAS_DUALSYSTEM_PARTITIONS := true
-RECOVERY_USE_PIXEL_FORMAT_BGR565 := true
 TARGET_RECOVERY_FSTAB = device/xiaomi/mione_plus/ramdisk/fstab.mione
 RECOVERY_FSTAB_VERSION := 2
-PRODUCT_COPY_FILES += \
-    device/xiaomi/mione_plus/recovery.fstab:recovery/root/etc/miui.fstab \
-    device/xiaomi/mione_plus/recovery/etc/fstab:recovery/root/etc/fstab \
-    device/xiaomi/mione_plus/recovery/device.conf:recovery/root/res/device.conf
 
 #lcd
 PRODUCT_PROPERTY_OVERRIDES += \
