78655977090830fa64e4af109996779a224f237e
PeterCxy <xqsx43cxy@126.com>
2013-10-27 13:03:12 +0800

    mione_plus: update new kernel cmdline

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,7 @@ TARGET_BOOTLOADER_BOARD_NAME := mione
 
 # Kernel
 BOARD_KERNEL_BASE := 0x40200000
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=mione
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=mione kgsl.mmutype=gpummu vmalloc=400M
 BOARD_KERNEL_PAGE_SIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01200000
 TARGET_KERNEL_CONFIG := mione_plus_defconfig
